f78799a1f06529403e3fbfe6c012b9e5516150d5
Ян Ветров <yan@MacBook-Pro-An.local>
2020-11-18 16:51:22 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.advantages-slider-wrapper').slick({
+        centerMode: true,
+        centerPadding: '30px',
+        infinite: true,
+        speed: 100,
+        arrows:true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        // prevArrow: $('.prev'),
+        // nextArrow: $('.next'),
+    });
+});
